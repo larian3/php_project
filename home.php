650a38d3ba0b36fd5f56b40458bb37a198956efb
larian3 <larianelima22@gmail.com>
2024-12-09 21:16:37 -0300

    project starting

---
+++ home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset= "UTF-8">
+        <meta http-equiv= "X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=0.1">
+        <title> home </title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+    <section class="header">
+        <a href="home.php" class="logo">travel</a>
+    </section>
+
+
+
+    <link src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js">
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    </body>
+</html>
